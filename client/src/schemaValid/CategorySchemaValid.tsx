5a8ce8d3c4be65b989ce585a05fe78d16e4374d9
andinhle <andinhle163@gmail.com>
2024-07-18 22:38:39 +0700

    categorys crud done

---
+++ client/src/schemaValid/CategorySchemaValid.tsx
@@ -0,0 +1,8 @@
+import { z } from "zod"
+const CategorySchemaValid =z.object({
+    name:z.string().min(6,"Tên danh mục tối thiểu 6 kí tự"),
+    slug:z.string().optional(),
+    status:z.boolean().optional()
+})
+
+export default CategorySchemaValid
