c60c24eb49328fecd96127f84aada756c7b72e33
Duanle250103 <lduan1656@gmail.com>
2024-07-25 12:25:56 +0700

    dangki

--- client/src/components/User/Header.tsx
+++ client/src/components/User/Header.tsx
@@ -1,8 +1,42 @@
+import { useContext } from "react"
+import { AuthContext, AuthContextType } from "../../context/AuthContext"
+import { Link } from "react-router-dom";
 
 
 const Header = () => {
+  const { user , logout} = useContext(AuthContext) as AuthContextType;
   return (
-    <div>Header</div>
+    <div>
+      <ul>
+				<li>
+					<Link to="/">Home</Link>
+				</li>
+				{user ? (
+					<>
+						<li>
+							<span>Welcome, {user?.email}</span>
+						</li>
+						<li>
+							<button onClick={logout}>Logout</button>
+						</li>
+						{user.role === "admin" && (
+							<li>
+								<Link to="/admin">Admin</Link>
+							</li>
+						)}
+					</>
+				) : (
+					<>
+						<li>
+							<Link to="/login">Login</Link>
+						</li>
+						<li>
+							<Link to="/register">Register</Link>
+						</li>
+					</>
+				)}
+			</ul>
+    </div>
   )
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,25 +4,26 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LayoutAdmin from "./pages/admin/LayoutAdmin.tsx";
-import Register from "./pages/user/Register.tsx";
 import LayoutUser from "./pages/user/LayoutUser.tsx";
-import Login from "./pages/user/Login.tsx";
 import CategorysAdmin from "./pages/admin/CategorysAdmin.tsx";
 import ProductsAdmin from "./pages/admin/ProductsAdmin.tsx";
 import CategorysProvider from "./context/CategoryContext.tsx";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import AuthForm from "./pages/user/AuthForm.tsx";
+import { AuthProvider } from "./context/AuthContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
     <CategorysProvider>
+      <AuthProvider>
       <Routes>
         <Route path="" element={<App />}>
           {/* User */}
           <Route element={<LayoutUser />}>
-            <Route path="register" element={<Register />} />
-            <Route path="login" element={<Login />} />
+            <Route path="/register" element={<AuthForm />} />
+            <Route path="/login" element={<AuthForm isLogin />} />
           </Route>
           {/* Admin */}
           <Route path="admin" element={<LayoutAdmin />}>
@@ -31,6 +32,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
           </Route>
         </Route>
       </Routes>
+      </AuthProvider>
       </CategorysProvider>
       <ToastContainer autoClose={3000} newestOnTop />
     </BrowserRouter>
