25e76d226dfa44106fe888406e665f565a8aaf0b
HoangHa2004 <156032980+HoangHa2004@users.noreply.github.com>
2024-07-27 12:49:22 +0700

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,61 +4,45 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LayoutAdmin from "./pages/admin/LayoutAdmin.tsx";
+import Register from "./pages/user/Register.tsx";
 import LayoutUser from "./pages/user/LayoutUser.tsx";
+import Login from "./pages/user/Login.tsx";
 import CategorysAdmin from "./pages/admin/CategorysAdmin.tsx";
 import ProductsAdmin from "./pages/admin/ProductsAdmin.tsx";
 import CategorysProvider from "./context/CategoryContext.tsx";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import AuthForm from "./pages/user/AuthForm.tsx";
-import { AuthProvider } from "./context/AuthContext.tsx";
 import ProductProvider from "./context/ProductContext.tsx";
 import HomePage from "./components/User/HomePage.tsx";
-import BlogsAdmin from "./pages/admin/BlogsAdmin.tsx";
-import BlogProvider from "./context/BlogContext.tsx";
-import UpdateBlogAdmin from "./pages/admin/UpdateBlogAdmin.tsx";
 import BlogPage from "./components/User/BlogPage.tsx";
-import DetailProduct from "./components/User/DetailProduct.tsx";
-import ProductList from "./components/User/ProductListPage.tsx";
-import ScrollToTop from "./utils/ScrollTop.tsx";
-
-import NewsPage from "./components/User/NewsPage.tsx";
-
+import ContactUs from "./components/User/ContactUs.tsx";
+import DetailBlog from "./components/User/DetailBlog.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
+      <CategorysProvider>
         <ProductProvider>
-          <CategorysProvider>
-            <BlogProvider>
-              <ScrollToTop />
-              <Routes>
-                <Route path="" element={<App />}>
-                  {/* User */}
-                  <Route element={<LayoutUser />}>
-                    <Route index element={<HomePage />} />
-                    <Route path="blogs" element={<BlogPage />} />
-                    <Route path="/news" element={<NewsPage />} />
-                    <Route path="detail/:slug" element={<DetailProduct />} />
-                    <Route path="products" element={<ProductList />} />
-                    <Route path="products/:slug" element={<ProductList />} />
-                    <Route path="register" element={<AuthForm />} />
-                    <Route path="login" element={<AuthForm isLogin />} />
-                  </Route>
-                  {/* Admin */}
-                  <Route path="admin" element={<LayoutAdmin />}>
-                    <Route path="categorys" element={<CategorysAdmin />} />
-                    <Route path="products" element={<ProductsAdmin />} />
-                    <Route path="blogs" element={<BlogsAdmin />} />
-                    <Route path="blogs/:_id" element={<UpdateBlogAdmin />} />
-                  </Route>
-                </Route>
-              </Routes>
-            </BlogProvider>
-          </CategorysProvider>
+          <Routes>
+            <Route path="" element={<App />}>
+              {/* User */}
+              <Route element={<LayoutUser />}>
+                <Route index element={<HomePage />} />
+                <Route path="/blogs" element={<BlogPage />} />
+                <Route path="/detailblog" element={<DetailBlog />} />
+                <Route path="/contactus" element={<ContactUs />} />
+                <Route path="register" element={<Register />} />
+                <Route path="login" element={<Login />} />
+              </Route>
+              {/* Admin */}
+              <Route path="admin" element={<LayoutAdmin />}>
+                <Route path="categorys" element={<CategorysAdmin />} />
+                <Route path="products" element={<ProductsAdmin />} />
+              </Route>
+            </Route>
+          </Routes>
         </ProductProvider>
-      </AuthProvider>
+      </CategorysProvider>
       <ToastContainer autoClose={3000} newestOnTop />
     </BrowserRouter>
   </React.StrictMode>
